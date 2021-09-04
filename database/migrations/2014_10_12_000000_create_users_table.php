a0539297dfaab1461b44dbb6e98a5df7d07015f5
DESKTOP-MUJTGOT\Ibn-e-Yunus <salmanabbas985@gmail.com>
2021-09-04 03:42:38 -0700

    is_admin added in users table and added Middleware

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_admin')->default(0); //added admin field
             $table->string('two_factor_code')->nullable(); //for two factor code
             $table->dateTime('two_factor_expires_at')->nullable(); //for two factor expiration
             $table->rememberToken();
